d735dd85246d09e700d034acdcac6f6d329e51cd
WitoldSierka <witsierka@gmail.com>
2025-09-05 13:31:03 +0200

    user defined history range

--- main.py
+++ main.py
@@ -12,20 +12,18 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-#images gallery
 last_images = []
-
+history_range = 50
 delete_message = True
 
 TYPES_OF_INTEREST = ["image", "video"]
-#audio?
 
 bot = commands.Bot(command_prefix='!', intents=intents)
+
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}")
-
 
 
 @bot.event
@@ -34,7 +32,7 @@ async def on_message(message):
     if message.author == bot.user:
         return
     temp = []
-    async for mess in message.channel.history(limit=30, oldest_first=False):
+    async for mess in message.channel.history(limit=history_range, oldest_first=False):
         if mess.author == bot.user or message == mess:
             continue
         for old_att in mess.attachments:
@@ -97,7 +95,28 @@ async def del_msg_off(ctx):
 async def manual(ctx):
     await ctx.send("This bot removes messages with reposted images or videos.\n"
                    "Warning-only mode can be toggled with the commands below:\n"
-                   "!del_msg_on - deletes message\n"
-                   "!del_msg_off - replies with a warning")
+                   "!del_msg_on   - deletes message\n"
+                   "!del_msg_off  - replies with a warning\n"
+                   "!set_range    - set how many past messages the bot will check for reposts\n"
+                   "!print_config - displays current settings")
+
+@bot.command()
+async def set_range(ctx, arg):
+    global history_range
+    try:
+        new_range = int(arg)
+    except ValueError:
+        await ctx.send("Range must be a valid, positive integer.")
+    else:
+        if new_range > 0:
+            history_range = new_range
+        else:
+            await ctx.send("Range must be a valid, positive integer.")
+
+@bot.command()
+async def print_config(ctx):
+    global delete_message, history_range
+    msg = "Current mode: " + "Message removal" if delete_message else "Send warning"
+    await ctx.send(f"Current history range: {history_range}\n{msg}")
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
